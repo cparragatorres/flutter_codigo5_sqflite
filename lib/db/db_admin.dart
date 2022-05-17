ff6ed750cc39c94dc190823b7878dcb2b6664f8d
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-05-16 20:04:48 -0500

    DB Init

---
+++ lib/db/db_admin.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DBAdmin {
+  Database? myDatabase;
+  static final DBAdmin db = DBAdmin._();
+  DBAdmin._();
+
+  getCheckDatabase() async{
+    if (myDatabase != null) return myDatabase;
+    myDatabase = await initDB(); //Creación de la base de datos
+    return myDatabase;
+  }
+
+   Future<Database> initDB() async {
+    Directory directory = await getApplicationDocumentsDirectory();
+    String path = join(directory.path, "BookDB.db");
+    return await openDatabase(
+      path,
+      version: 1,
+      onOpen: (db) {},
+      onCreate: (Database db, int v) async {
+        await db.execute("CREATE TABLE...");
+      },
+    );
+  }
+}
