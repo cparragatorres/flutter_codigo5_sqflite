c30adfe8e1f1c56e5ed46711dd67aaf316711187
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-05-16 20:23:08 -0500

    DB Init 3

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_codigo5_sqflite/db/db_admin.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,6 +10,14 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    DBAdmin.db.initDB();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold();
