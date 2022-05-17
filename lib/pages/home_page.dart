05aa932c4d8d263dd311d1a4449af3186874d75c
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-05-16 22:28:05 -0500

    UI 1

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_codigo5_sqflite/db/db_admin.dart';
+import 'package:flutter_codigo5_sqflite/ui/utils/colors.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -29,24 +31,73 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
-
-
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Books"),
+      backgroundColor: kPrimaryColor,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Good Morning",
+                            style: GoogleFonts.poppins(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w400,
+                              fontSize: 14.0
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 4.0,
+                          ),
+                          Text(
+                            "Fiorella de Fátima",
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: GoogleFonts.poppins(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w500,
+                                fontSize: 20.0
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(10.0),
+                      child: Image.network(
+                        "https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+                        height: 64,
+                        width: 64,
+                        fit: BoxFit.cover,
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
-      body: ListView.builder(
-        itemCount: books.length,
-        itemBuilder: (BuildContext context, int index){
-          return ListTile(
-            title: Text(books[index]["title"]),
-          );
-        },
-      ),
+
+
+
+      // body: ListView.builder(
+      //   itemCount: books.length,
+      //   itemBuilder: (BuildContext context, int index){
+      //     return ListTile(
+      //       title: Text(books[index]["title"]),
+      //     );
+      //   },
+      // ),
       // body: FutureBuilder(
       //   future: DBAdmin.db.getBooksRaw(),
       //   builder: (BuildContext context, AsyncSnapshot snap){
---
+++ lib/ui/utils/colors.dart
@@ -0,0 +1,4 @@
+
+import 'dart:ui';
+
+const Color kPrimaryColor = Color(0xff1D202B);
