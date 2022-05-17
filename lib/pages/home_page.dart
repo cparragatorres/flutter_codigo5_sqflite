7d68be2563449abd4872ae6b23b36a67d8fe8354
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-05-16 23:03:56 -0500

    UI 4

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -155,6 +155,59 @@ class _HomePageState extends State<HomePage> {
                     ),
                   ],
                 ),
+                const SizedBox(
+                  height: 30.0,
+                ),
+
+                SizedBox(
+                  width: 170,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(14.0),
+                        child: Image.network(
+                          "https://bookstr.com/wp-content/uploads/2019/12/Image-via-Amazon-1.jpg",
+                          height: 250,
+                          width: 170,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 6.0,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "JRR Tolkien",
+                              style: GoogleFonts.poppins(
+                                fontSize: 12.0,
+                                color: Colors.white70,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 2.0,
+                            ),
+                            Text(
+                              "The Hobbit - Final Edition",
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              style: GoogleFonts.poppins(
+                                fontSize: 14.0,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
               ],
             ),
           ),
