b88a179262b4ca9db3e2010ac4af67ce0aa67e48
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-05-17 19:32:13 -0500

    UI 8

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -165,56 +165,21 @@ class _HomePageState extends State<HomePage> {
                 const SizedBox(
                   height: 30.0,
                 ),
-                Row(
-                  children: [
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(10.0),
-                      child: Image.network(
-                        "https://images-na.ssl-images-amazon.com/images/I/61pMvyfWphL.jpg",
-                        width: 76.0,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 12.0,
-                    ),
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "Gabriel García Marquez",
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                            style: GoogleFonts.poppins(
-                              color: Colors.white60,
-                              fontSize: 12.0,
-                            ),
-                          ),
-                          Text(
-                            "Cien años de soledad",
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                            style: GoogleFonts.poppins(
-                              color: Colors.white,
-                              fontSize: 16.0,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                          Text(
-                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-                            maxLines: 3,
-                            overflow: TextOverflow.ellipsis,
-                            style: GoogleFonts.poppins(
-                              color: Colors.white60,
-                              fontSize: 12.0,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
+                ItemBookWidget(),
+                ItemBookWidget(),
+                ItemBookWidget(),
+                ItemBookWidget(),
+                ItemBookWidget(),
+                ItemBookWidget(),
+                ItemBookWidget(),
+                ItemBookWidget(),
+                ItemBookWidget(),
+                ItemBookWidget(),
+                ItemBookWidget(),
+                ItemBookWidget(),
+                ItemBookWidget(),
+                ItemBookWidget(),
+                ItemBookWidget(),
               ],
             ),
           ),
@@ -246,6 +211,69 @@ class _HomePageState extends State<HomePage> {
       //     return Text("ss");
       //   },
       // ),
+    );
+  }
+}
+
+class ItemBookWidget extends StatelessWidget {
+  const ItemBookWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 16.0),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10.0),
+            child: Image.network(
+              "https://images-na.ssl-images-amazon.com/images/I/61pMvyfWphL.jpg",
+              width: 76.0,
+              fit: BoxFit.cover,
+            ),
+          ),
+          const SizedBox(
+            width: 12.0,
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Gabriel García Marquez",
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: GoogleFonts.poppins(
+                    color: Colors.white60,
+                    fontSize: 12.0,
+                  ),
+                ),
+                Text(
+                  "Cien años de soledad",
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: GoogleFonts.poppins(
+                    color: Colors.white,
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                Text(
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                  maxLines: 3,
+                  overflow: TextOverflow.ellipsis,
+                  style: GoogleFonts.poppins(
+                    color: Colors.white60,
+                    fontSize: 12.0,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
