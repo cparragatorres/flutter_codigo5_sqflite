5cda2195fd2dd98ffefde75d73da8400edfd69c5
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-05-17 19:13:16 -0500

    UI 5

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -156,57 +156,22 @@ class _HomePageState extends State<HomePage> {
                   ],
                 ),
                 const SizedBox(
-                  height: 30.0,
+                  height: 20.0,
                 ),
 
-                SizedBox(
-                  width: 170,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                SingleChildScrollView(
+                  physics: const BouncingScrollPhysics(),
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
                     children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(14.0),
-                        child: Image.network(
-                          "https://bookstr.com/wp-content/uploads/2019/12/Image-via-Amazon-1.jpg",
-                          height: 250,
-                          width: 170,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 6.0,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "JRR Tolkien",
-                              style: GoogleFonts.poppins(
-                                fontSize: 12.0,
-                                color: Colors.white70,
-                              ),
-                            ),
-                            const SizedBox(
-                              height: 2.0,
-                            ),
-                            Text(
-                              "The Hobbit - Final Edition",
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                              style: GoogleFonts.poppins(
-                                fontSize: 14.0,
-                                color: Colors.white,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
+                      ItemSliderWidget(),
+                      ItemSliderWidget(),
+                      ItemSliderWidget(),
+                      ItemSliderWidget(),
                     ],
                   ),
                 ),
+
 
               ],
             ),
@@ -241,6 +206,65 @@ class _HomePageState extends State<HomePage> {
       //     return Text("ss");
       //   },
       // ),
+    );
+  }
+}
+
+class ItemSliderWidget extends StatelessWidget {
+  const ItemSliderWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 170,
+      margin: const EdgeInsets.only(right: 14.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(14.0),
+            child: Image.network(
+              "https://bookstr.com/wp-content/uploads/2019/12/Image-via-Amazon-1.jpg",
+              height: 250,
+              width: 170,
+              fit: BoxFit.cover,
+            ),
+          ),
+          const SizedBox(
+            height: 6.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "JRR Tolkien",
+                  style: GoogleFonts.poppins(
+                    fontSize: 12.0,
+                    color: Colors.white70,
+                  ),
+                ),
+                const SizedBox(
+                  height: 2.0,
+                ),
+                Text(
+                  "The Hobbit - Final Edition",
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: GoogleFonts.poppins(
+                    fontSize: 14.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
